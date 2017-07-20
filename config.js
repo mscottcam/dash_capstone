a94523c79ec0f93e9ea3bafff4eba6def6a4b021
dankato <apearmor@gmail.com>
2017-07-20 09:34:06 -0700

    installed dotenv

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 'use strict';
+
+require('dotenv').config();
+
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/dash-app';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL || 'mongodb://localhost/dash-app');
