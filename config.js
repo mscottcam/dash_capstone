60abb2c8cad461fdb8167de3b03b9c7f7b505ac1
mscottcam <mscottcam@gmail.com>
2017-07-20 11:12:06 -0500

    travis testing thursday morning

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://dev:password@ds123371.mlab.com:23371/dash-app';
 exports.TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL || 'mongodb://localhost/dash-app');
+	process.env.TEST_DATABASE_URL || 'mongodb://dev:password@ds123371.mlab.com:23371/dash-app');
 exports.PORT = process.env.PORT || 8080;
+
+
+// mongodb://localhost/dash-app
